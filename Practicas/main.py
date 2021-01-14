9323362399e6ab6b8ea9c0299d7b016ad76b832a
nandoferth <vizcainolopezfernando1996@outlook.es>
2021-01-13 21:43:34 -0600

    Tabla LL1

---
+++ Practicas/main.py
@@ -0,0 +1,11 @@
+from LL1 import ll1
+
+def main():
+    Ll1 = ll1()
+    #print(Ll1.get_producciones()[0].keys())
+    #print(Ll1.get_producciones()[1].values())
+    #print(Ll1.logica('A'))
+    #print(Ll1.get_no_terminales(), Ll1.get_terminales())
+    print(Ll1.recorre_producciones())
+    print(Ll1.get_tablaLL1())
+main()
